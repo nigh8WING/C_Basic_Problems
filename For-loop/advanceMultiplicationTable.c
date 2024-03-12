fb1a8ebab2736cbf8febb422e8b7251365fd0cdb
nigh8WING <163192028+nigh8WING@users.noreply.github.com>
2024-03-13 03:30:48 +0600

    Create advanceMultiplicationTable.c
    Write a program in C to display the multiplier table vertically from 1 to n.
    Test Data :
    Input upto the table number starting from 1 : 8
    Expected Output :
    Multiplication table from 1 to 8
    1x1 = 1, 2x1 = 2, 3x1 = 3, 4x1 = 4, 5x1 = 5, 6x1 = 6, 7x1 = 7, 8x1 = 8
    ...
    1x10 = 10, 2x10 = 20, 3x10 = 30, 4x10 = 40, 5x10 = 50, 6x10 = 60, 7x10 = 70, 8x10 = 80

---
+++ For-loop/advanceMultiplicationTable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, j;
+    printf("Input upto the table number starting from 1 : ");
+    scanf("%d", &n);
+  
+    int multiplier = 0;
+
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++){
+            multiplier = i * j;
+            printf("%d X %d = %d, ", i, j, multiplier);
+        }
+    }
+    return 0;
+}
