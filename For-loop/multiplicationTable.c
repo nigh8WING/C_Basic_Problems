811aa2fd57209f1439622a52048f6ffb39e1a8f3
nigh8WING <163192028+nigh8WING@users.noreply.github.com>
2024-03-13 03:26:24 +0600

    Update multiplicationTable.c
    Write a program in C to display the multiplication table for a given integer.
    Test Data :
    Input the number (Table to be calculated) : 15
    Expected Output :
    15 X 1 = 15
    ...
    ...
    15 X 10 = 150

--- For-loop/multiplicationTable.c
+++ For-loop/multiplicationTable.c
@@ -2,13 +2,11 @@
 
 int main()
 {
-    int n;
+    int n, i;
     printf("Input the number (Table to be calculated) : ");
     scanf("%d", &n);
 
     int value = n;
-
-    int i;
 
     for(i = 1; i <= 10; i++){
         value = n * i;
